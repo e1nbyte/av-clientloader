f074007e0d26f0757f8aefe16ed27c8214fc5919
E1nbyte <111838980+e1nbyte@users.noreply.github.com>
2024-10-31 13:47:58 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 lua54 'yes'
 
-author 'E1nbyte <https://github.com/e1nbyte>'
+author 'E1nbyte (https://github.com/e1nbyte)'
 description 'Free Open Source Client Loader for FiveM, Anti exploitation mechanism'
 version '1.0.0'
 
